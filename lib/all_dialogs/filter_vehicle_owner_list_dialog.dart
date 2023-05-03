bb5a8281ba0248cea40d6a6e7923db8369ce96a2
AshishCodexxa <ashish@codexxa.in>
2023-05-03 10:38:51 +0530

    add

---
+++ lib/all_dialogs/filter_vehicle_owner_list_dialog.dart
@@ -0,0 +1,206 @@
+
+import 'package:fixgotransporterapp/common_file/common_color.dart';
+import 'package:fixgotransporterapp/common_file/size_config.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class VehicleOwnerListFilter extends StatefulWidget {
+  const VehicleOwnerListFilter({Key? key}) : super(key: key);
+
+  @override
+  State<VehicleOwnerListFilter> createState() => _VehicleOwnerListFilterState();
+}
+
+class _VehicleOwnerListFilterState extends State<VehicleOwnerListFilter> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Padding(
+      padding:
+      EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+          height: SizeConfig.screenHeight * 0.3,
+          color: Colors.transparent,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
+                )),
+            child: Column(
+              children: [
+
+                Container(
+                  height: SizeConfig.screenHeight*0.07,
+                  decoration: BoxDecoration(
+                    color: CommonColor.SIGN_UP_TEXT_COLOR,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)
+                    )
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.05, right: SizeConfig.screenWidth*0.05),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            color: Colors.transparent,
+                            child: Icon(Icons.arrow_back_ios_new,
+                              color: Colors.transparent,)),
+                        Padding(
+                          padding: EdgeInsets.only(left: SizeConfig.screenHeight * 0.01),
+                          child: Text("Sort By",
+                            style: TextStyle(
+                                fontSize: SizeConfig.blockSizeHorizontal*5.0,
+                                fontFamily: "Roboto_Medium",
+                                fontWeight: FontWeight.w500,
+                                color: CommonColor.WHITE_COLOR
+                            ),),
+                        ),
+                        GestureDetector(
+                          onDoubleTap: (){},
+                          onTap: (){
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            color: Colors.transparent,
+                            child: const Icon(Icons.clear,
+                              color: Colors.white,
+                              ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+
+                Padding(
+                  padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.05, top: SizeConfig.screenHeight*0.02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: SizeConfig.screenHeight*0.03,
+                        width: SizeConfig.screenWidth*0.068,
+                        decoration:  BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          shape: BoxShape.circle
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.03),
+                        child: Text(
+                            "Lowest Rate",
+                            style: TextStyle(
+                                fontSize:
+                                SizeConfig.blockSizeHorizontal * 4.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                fontFamily: 'Roboto_Regular')
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(top: SizeConfig.screenHeight*0.02),
+                  child: Container(
+                    height: SizeConfig.screenWidth*0.003,
+                    color: Colors.black12,
+                    child: Row(
+                      children: const [
+                        Text("hii",
+                          style: TextStyle(
+                              color: Colors.transparent
+                          ),),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.05, top: SizeConfig.screenHeight*0.02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: SizeConfig.screenHeight*0.03,
+                        width: SizeConfig.screenWidth*0.068,
+                        decoration:  BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            shape: BoxShape.circle
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.03),
+                        child: Text(
+                            "Rating / Reviews",
+                            style: TextStyle(
+                                fontSize:
+                                SizeConfig.blockSizeHorizontal * 4.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                fontFamily: 'Roboto_Regular')
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(top: SizeConfig.screenHeight*0.02),
+                  child: Container(
+                    height: SizeConfig.screenWidth*0.003,
+                    color: Colors.black12,
+                    child: Row(
+                      children: const [
+                        Text("hii",
+                          style: TextStyle(
+                              color: Colors.transparent
+                          ),),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.05, top: SizeConfig.screenHeight*0.02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: SizeConfig.screenHeight*0.03,
+                        width: SizeConfig.screenWidth*0.068,
+                        decoration:  BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            shape: BoxShape.circle
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.03),
+                        child: Text(
+                            "Availability Date",
+                            style: TextStyle(
+                                fontSize:
+                                SizeConfig.blockSizeHorizontal * 4.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                fontFamily: 'Roboto_Regular')
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ],
+            )
+          )),
+    );
+  }
+}
--- lib/presentation/my_post_module/my_post_child_screen.dart
+++ lib/presentation/my_post_module/my_post_child_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fixgotransporterapp/common_file/common_color.dart';
 import 'package:fixgotransporterapp/common_file/size_config.dart';
+import 'package:fixgotransporterapp/presentation/my_post_module/pending_post_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -20,14 +22,100 @@ class _MyPostChildScreenState extends State<MyPostChildScreen> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      body: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(SizeConfig.screenHeight * 0.13),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    color: CommonColor.APP_BAR_COLOR,
+                    height: SizeConfig.screenHeight*0.12,
+                    child: TabBar(
+                        isScrollable: true,
+                        padding: EdgeInsets.only(left: SizeConfig.screenWidth*0.05,
+                            right: SizeConfig.screenWidth*0.05,
+                            top: SizeConfig.screenHeight*0.07),
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        unselectedLabelColor: Colors.white,
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto_Medium',
+                            fontSize: SizeConfig.blockSizeHorizontal*4.0
+                        ),
+                        unselectedLabelStyle: TextStyle(
+                            fontSize: SizeConfig.blockSizeHorizontal * 4.0,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Roboto_Regular',
+                            color: Colors.white
+                        ),
+                        indicator: BoxDecoration(
+                          border: Border(bottom: BorderSide(color: Colors.white,
+                              width: SizeConfig.screenWidth*0.005) ),
+                        ),
+                        tabs: const [
+                          Tab(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Pending',
+                              ),
+                            ),
+                          ),
+                          Tab(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'On Going',
+                              ),
+                            ),
+                          ),
+                          Tab(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Completed',
+                              ),
+                            ),
+                          ),
+                          Tab(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Cancelled',
+                              ),
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    color: CommonColor.APP_BAR_COLOR,
+                    height: SizeConfig.screenHeight*0.007,
+                  ),
+                ],
+              ),
+            ),
+            body: Container(
+              color: Colors.transparent,
+              height: SizeConfig.screenHeight*0.9,
+              child: TabBarView(
+                children: <Widget>[
+                 PendingPostScreen(),
+                 Container(),
+                 Container(),
+                 Container(),
+                ],
+              ),
+            )),
       ),
     );
   }
 }
+
 
 
 abstract class MyPostChildScreenListener{
